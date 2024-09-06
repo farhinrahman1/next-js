638c9d810b51513e1d07111172a421bdd7339e7f
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-09-06 23:37:19 +0600

    added desc

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold text-center sm:text-left">Welcome to Next.js 2024</h1>
+    <div className="items-center justify-between min-h-screen p-12 flex flex-col">
+      <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">Welcome to Next.js 2024</h1>
       </main>
       
     </div>
